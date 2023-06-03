756256980ca6093c62fdef6fa325f2cbe694d92e
John Doe <johndoe@email.com>
2023-06-03 21:59:54 +0700

    ...

--- src/helpers/imagebannerlargelist.js
+++ src/helpers/imagebannerlargelist.js
@@ -4,21 +4,21 @@ import ChildrenBannerLarge from "../assets/childbannerlarge.jpg"
 
 export const ImageBannerLargeList = [
     {
-        link: "/men",
+        link: "/mock-react-website/men",
         title: "Men's Collection",
         description: "Elegant, noble. Handcrafted to perfection. Only at Vouis Le Trouix.",
         image: MenBannerLarge,
         button: "Explore Men's Collection"
     },
     {
-        link: "/women",
+        link: "/mock-react-website/women",
         title: "Women's Collection",
         description: "Glamorous, delicate. Handcrafted to perfection. Only at Vouis Le Trouix.",
         image: WomenBannerLarge,
         button: "Explore Women's Collection"
     },
     {
-        link: "/children",
+        link: "/mock-react-website/children",
         title: "Children's Collection.",
         description: "Bring your joy to your kids too. Only at Vouis Le Troiux.",
         image: ChildrenBannerLarge,

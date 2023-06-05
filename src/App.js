95e1f156b971b8ab80ae390f3c93f30e041412bb
John Doe <johndoe@email.com>
2023-06-05 21:59:32 +0700

    ...

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom'
+import { Suspense } from 'react';
 import Navbar from './components/navbar.js'
 import Home from './site/Home';
 import News from './site/news';
@@ -13,25 +14,36 @@ import Contact from './site/Contact';
 import Login from './site/Login';
 import Signup from './site/Signup';
 import Footer from './components/Footer';
+import { Bars } from 'react-loader-spinner';
+
+function Loader() {
+  return (
+    <div className='loader-container'>
+      <Bars height="80" width="80" color="#ffffff" ariaLabel="bars-loading" wrapperStyle={{}} wrapperClass="" visible={true}/>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div style={{height: 92 + 'px', width: 100 + '%', backgroundColor: 'black'}}></div>
-      <Routes>
-          <Route path="mock-react-website/" element={<Home />}></Route>
-          <Route path="mock-react-website/news" element={<News />}></Route>
-          <Route path="mock-react-website/shirts" element={<Shirts />}></Route>
-          <Route path="mock-react-website/trousers" element = {<Trousers />}></Route>
-          <Route path="mock-react-website/women" element = {<Women />}></Route>
-          <Route path="mock-react-website/men" element={<Men/ >}></Route>
-          <Route path="mock-react-website/handbags" element={<Handbags/ >}></Route>
-          <Route path="mock-react-website/children" element={<Children/ >}></Route>
-          <Route path="mock-react-website/contact" element={<Contact/ >}></Route>
-          <Route path="mock-react-website/login" element={<Login/ >}></Route>
-          <Route path="mock-react-website/signup" element={<Signup/ >}></Route>
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+            <Route path="mock-react-website/" element={<Home />}></Route>
+            <Route path="mock-react-website/news" element={<News />}></Route>
+            <Route path="mock-react-website/shirts" element={<Shirts />}></Route>
+            <Route path="mock-react-website/trousers" element = {<Trousers />}></Route>
+            <Route path="mock-react-website/women" element = {<Women />}></Route>
+            <Route path="mock-react-website/men" element={<Men/ >}></Route>
+            <Route path="mock-react-website/handbags" element={<Handbags/ >}></Route>
+            <Route path="mock-react-website/children" element={<Children/ >}></Route>
+            <Route path="mock-react-website/contact" element={<Contact/ >}></Route>
+            <Route path="mock-react-website/login" element={<Login/ >}></Route>
+            <Route path="mock-react-website/signup" element={<Signup/ >}></Route>
+        </Routes>
+      </Suspense>
       <Footer />
     </div>
   );
--- src/site/Men.js
+++ src/site/Men.js
@@ -1,9 +1,71 @@
-import React from "react";
+import "../App.css";
+import { Canvas } from "@react-three/fiber";
+// import Model from "./components/Model";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import * as core from "@theatre/core";
+// import studio from "@theatre/studio";
+import {getProject} from '@theatre/core';
+import extension from '@theatre/r3f/dist/extension';
+import { SheetProvider } from "@theatre/r3f";
+import {editable as e} from '@theatre/r3f';
+import demostate from './state.json';
 
-function Men() {
-    return (
-        <div>Men page</div>
-    )
+// studio.initialize();
+// studio.extend(extension)
+
+export default function Men() {
+  const demoSheet = getProject('Demo Project', {state: demostate}).sheet('Demo Sheet');
+  const object = useGLTF("./tommy_hilfiger_jacket/scene.gltf").scene;
+  const html = document.documentElement;
+  window.addEventListener("scroll", e => {
+    const currentScroll = html.scrollTop;
+    const maxScroll = html.scrollHeight - window.innerHeight;
+    const timeIndex = currentScroll / maxScroll * 15;
+    const saection = document.querySelector("#section");
+    demoSheet.sequence.position = timeIndex;
+    saection.style.opacity = 1;
+
+    if (Math.round(demoSheet.sequence.position) == 2) {
+      saection.innerText = "Introducing";
+      saection.style.opacity = 1;
+    } else if (Math.round(demoSheet.sequence.position) == 4) {
+      saection.innerText = "Long sleeve made with leather.";
+      saection.style.opacity = 1;
+    } else if (Math.round(demoSheet.sequence.position) == 6) {
+      saection.innerText = "Strongful zipper, stronger than a bull.";
+      saection.style.opacity = 1;
+    } else if (Math.round(demoSheet.sequence.position) == 8) {
+      saection.innerText = "Two bags on the Jacket.";
+      saection.style.opacity = 1;
+    } else if (Math.round(demoSheet.sequence.position) == 10) {
+      saection.innerText = "Neck made out of soft leather.";
+      saection.style.opacity = 1;
+    } else if (Math.round(demoSheet.sequence.position) == 12) {
+      saection.innerText = "All Jackets here are made perfectly like that. Handmade.";
+      saection.style.opacity = 1;
+    } else if (Math.round(demoSheet.sequence.position) == 14) {
+      saection.innerText = "Only at Vouis Le Trouix.";
+      saection.style.opacity = 1;
+    } else if (Math.round(demoSheet.sequence.position) >= 15) {
+      document.querySelector(".canvas-container").style.position = "absolute";
+      saection.style.opacity = 0;
+    } else {
+      document.querySelector(".canvas-container").style.position = "fixed";
+      saection.style.opacity = 0;
+    }
+  })
+  return (
+    <div className="Men">
+      <div className="canvas-container">
+        <Canvas>
+          <SheetProvider sheet={demoSheet}>
+            <e.primitive theatreKey="object" editableType="mesh" object={object} position={[0, -5, 0]} />
+            <e.pointLight theatreKey="light" position={[0, 10, 10]} />
+            {/* <OrbitC ontrols /> */}
+          </SheetProvider>
+        </Canvas>
+      </div>
+      <div id="section"></div>
+    </div>
+  );
 }
-
-export default Men
